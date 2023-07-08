5869f2a4c1ee01429bf6d3dfe114f262e141a789
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-07-08 18:23:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="hackbc9419ab11eebe56",
-    version="0.0.1",
+    version="0.0.11",
     packages=find_packages(),
     install_requires=[
         "lxml",
